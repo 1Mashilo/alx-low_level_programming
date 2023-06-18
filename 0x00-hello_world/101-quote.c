c0746eb73ac865310506727fd8ebf93009c735e1
1Mashilo <Christophermashilo@gmail.com>
2023-06-18 02:03:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - printing withiut printf and puts
+* main - printing without printf and puts
 *
 * Return: always (1)
 *
 */
 int main(void)
 {
-	  const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n;	    write(STDERR_FILENO, message, sizeof(message) - 1);
-	      return 1;
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+		return (1);
 }

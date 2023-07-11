1500ccb113e8d1f05354220b38a1b6cb49405ea7
1Mashilo <Christophermashilo@gmail.com>
2023-07-11 12:52:26 -0700

    100-argstostr.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -53,11 +53,14 @@ int **alloc_grid(int width, int height)
 
 	for (x = 0; x < height; x++)
 
-	{
+{
 
-		for (y = 0; y < width; y++)
+for (y = 0; y < width; y++)
 
-			mee[x][y] = 0;
-	}
-	return (mee);
+mee[x][y] = 0;
+
+}
+
+return (mee);
+
 }

13a69d25d71efb6b926a0ea3166602064e4606d7
1Mashilo <Christophermashilo@gmail.com>
2023-07-31 14:42:46 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - Prints a string in reverse using recursion.
+* @s: Pointer to the string to be printed in reverse.
+* Description: This function prints the characters of the input string 's'
+* in reverse order recursively until the null terminator '\0'
+* is encountered. It uses recursion to navigate through the
+* string from the end to the beginning. After printing all
+* characters in reverse order, it stops at the null terminator
+* Return: None (void).
+*/
+
+void _print_rev_recursion(char *s)
+
+{
+if (*s == '\0')
+
+{
+return; 
+}
+
+_print_rev_recursion(s + 1); 
+
+putchar(*s); 
+}
+

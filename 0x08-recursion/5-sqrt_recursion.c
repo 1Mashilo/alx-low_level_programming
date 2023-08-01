58fe258365bfb08d83db7d64c3058a68ba8ac16b
1Mashilo <Christophermashilo@gmail.com>
2023-07-31 23:49:08 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,19 +14,25 @@
 * Return: The natural square root of 'n' as an integer.
 */
 int _sqrt_recursion(int n)
+
 {
+
 if (n < 0)
+
 {
 return (-1);
 }
 
 if (n == 0 || n == 1)
+
 {
 return (n);
 }
 
 int start = 1;
+
 int end = n;
+
 int mid, result;
 
 while (start <= end)

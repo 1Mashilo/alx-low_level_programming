6c3f8f0a995e5c2a26e7ffcfdec0b0d3ebfefee1
1Mashilo <christophermashilo@gmail.com>
2023-09-04 06:04:45 -0700

    second commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,32 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* read_textfile - Reads and prints the content of a text file to stdout
-* @filename: The name of the file to read
-* @letters: The number of letters to read and print
-* Return: The actual number of letters read and printed, or 0 on failure
+* read_textfile - Reads a text file and prints it to STDOUT.
+* @filename: The name of the text file to be read.
+* @letters: The number of letters to be read and printed.
+*
+* Return: The actual number of bytes read and printed (w).
+*         0 when the function fails or if filename is NULL.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
+char *buffer;
+ssize_t file_descriptor;
+ssize_t bytes_written;
+ssize_t total_bytes_read;
 
-int fd, bytes_read, bytes_written;
-char buffer[1024];
-
-if (!filename)
+file_descriptor = open(filename, O_RDONLY);
+if (file_descriptor == -1)
 return (0);
 
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
+buffer = malloc(sizeof(char) * letters);
+total_bytes_read = read(file_descriptor, buffer, letters);
+bytes_written = write(STDOUT_FILENO, buffer, total_bytes_read);
 
-bytes_read = read(fd, buffer, letters);
-if (bytes_read == -1)
-{
-close(fd);
-return (0);
-}
+free(buffer);
+close(file_descriptor);
 
-bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read)
-{
-close(fd);
-return (0);
-}
-
-close(fd);
 return (bytes_written);
 }
 

06a62e2eb055f971eb782392bc936d4bbb30e643
Mashilo Kgaladi <134360971+1Mashilo@users.noreply.github.com>
2024-04-10 16:39:13 +0000

    improved documentation

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,30 @@
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
+/*
+ * Function: linear_search
+ * -----------------------
+ * Searches for a specific value within a given array using linear search
+ *
+ * array: Pointer to the integer array to be searched
+ * size: Size of the array (number of elements)
+ * value: The value to search for within the array
+ *
+ * Return: The index of the found element, or -1 if not found
+ */
 int linear_search(int *array, size_t size, int value);
+
+/*
+ * Function: binary_search
+ * -----------------------
+ * Searches for a specific value within a SORTED array using binary search
+ *
+ * array: Pointer to the integer array to be searched
+ * size: Size of the array (number of elements)
+ * value: The value to search for within the array
+ *
+ * Return: The index of the found element, or -1 if not found
+ */
 int binary_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
